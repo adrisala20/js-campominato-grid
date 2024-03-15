6158407eceebbc3542b1577a39ce292ee45e5b3d
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-15 14:56:44 +0100

    add variable variable to button play

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 */
+
+/* <div class="boxes"></div> */
+
+//creo la variabile per prendere il bottone
+let bottone = document.querySelector('.btn');
+
+//aggiungo l'evento di ascolto al bottone
+bottone.addEventListener('click', function()) {
+
+    
+
+
+}
