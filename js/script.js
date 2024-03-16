e671f781b80fa1bb3d43fdbe5eac1321bbfc5f45
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-16 12:09:40 +0100

    add function in js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,29 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 */
 
-/* <div class="boxes"></div> */
+/* <div class="boxes"></div> 
+    <span> numero </span> */
 
+//inizio la funzione per creare un quadrato
+//aggiungere il tag div 
+//aggiungo la classe del mio quadrato
 
+function createNewBox (content){
+    const newBox = document.createElement('div');
+    newBox.classList.add('boxes');
+    newBox.innerHTML = '<span>' + content + '</span>'; 
+    return newBox;
 
-//mi sono bloccata, proseguo nel fine settimana
+}
+// console.log(createNewBox('ciao'))
+
+const elBox = document.getElementById('box');
+console.log( typeof elBox)
+
+//genero un ciclo per creare i quadrati
+for(let i=1; i <=100 ; i++){
+
+    let generateBox = createNewBox(i);
+    // elBox.append(newBox);
+
+}
