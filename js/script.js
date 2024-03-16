abd61789b1070e040db226fc83d979d649a18d2a
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-16 11:39:09 +0100

    started from adjusting html/css

--- js/script.js
+++ js/script.js
@@ -6,31 +6,6 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 
 /* <div class="boxes"></div> */
 
-//prendo il mio elemento da html
-let elBoxes = document.getElementById('boxes');
-console.log (typeof elBoxes)
 
-// creo una funzione per creare il div da inserire dentro il div boxes
-function generatedBox (){
-    let elBox = document.createElement ('div');
-    elBox.classList.add('box');
-    return elBox;
-}
-
-for(i=1 ; i < 100 ; i++){
-    let newBox = generatedBox();
-    elBoxes.appendChild(newBox);
-}
-
-//creo la variabile per prendere il bottone
-let bottone = document.querySelector('.btn');
-
-//aggiungo l'evento di ascolto al bottone
-bottone.addEventListener('click', function() {
-
- 
-
-
-});
 
 //mi sono bloccata, proseguo nel fine settimana
